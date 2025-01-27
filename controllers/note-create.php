6b21a8cd3ac7222a0fe618a74139af39044f74b4
Nick Melms <nick@craftedagency.com>
2025-01-27 12:18:43 -0500

    basic form validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,10 +6,20 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-  $db->query('insert into notes(body,user_id) values(:body, :user_id)', [
-    'body' => $_POST['body'],
-    'user_id' => 1,
-  ]);
+  $errors = [];
+
+  if(strlen($_POST['body']) === 0){
+    $errors['body'] = 'a body is required';
+  }else if(strlen($_POST['body']) > 100){
+    $errors['body'] = 'body cannot be more than 1000 characters';
+  }
+
+  if(empty($errors)){
+      $db->query('insert into notes(body,user_id) values(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1,
+      ]);
+  };
 }
 
 require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -9,20 +9,14 @@
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <!-- <div class="sm:col-span-4">
-            <label for="username" class="block text-sm/6 font-medium text-gray-900">Username</label>
-            <div class="mt-2">
-              <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                <div class="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">workcation.com/</div>
-                <input type="text" name="username" id="username" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="janesmith">
-              </div>
-            </div>
-          </div> -->
 
           <div class="col-span-full">
             <label for="body" class="block text-sm/6 font-medium text-gray-900">Body</label>
             <div class="mt-2">
-              <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+              <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"><?= $_POST['body'] ?? '' ?></textarea>
+              <?php if(isset($errors['body'])) : ?>
+                <p><?= $errors['body'] ?> </p>
+              <?php endif ?> 
             </div>
           </div>
         </div>
