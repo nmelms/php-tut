f998c6ffec5f9c88b6c68f11812b7e5fa45fd727
Nick Melms <nick@craftedagency.com>
2025-01-26 19:07:30 -0500

    remove . in link on nav

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,10 +8,10 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="./" class="<?= isURl('/') ? 'bg-gray-900 text-white' : 'text-gray-300'?> rounded-md  px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-              <a href="./about" class="<?= isUrl('/about') ? 'bg-gray-900 text-white' : 'text-gray-300'?> rounded-md px-3 py-2 text-sm font-medium ">About</a>
-              <a href="./contact" class="<?= isUrl('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300'?> rounded-md px-3 py-2 text-sm font-medium ">Contact</a>
-              <a href="./notes" class="<?= isUrl('/notes') ? 'bg-gray-900 text-white' : 'text-gray-300'?> rounded-md px-3 py-2 text-sm font-medium ">Notes</a>
+              <a href="/" class="<?= isURl('/') ? 'bg-gray-900 text-white' : 'text-gray-300'?> rounded-md  px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+              <a href="/about" class="<?= isUrl('/about') ? 'bg-gray-900 text-white' : 'text-gray-300'?> rounded-md px-3 py-2 text-sm font-medium ">About</a>
+              <a href="/contact" class="<?= isUrl('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300'?> rounded-md px-3 py-2 text-sm font-medium ">Contact</a>
+              <a href="/notes" class="<?= isUrl('/notes') ? 'bg-gray-900 text-white' : 'text-gray-300'?> rounded-md px-3 py-2 text-sm font-medium ">Notes</a>
             </div>
           </div>  
         </div>
