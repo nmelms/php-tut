c678d27b85f49265fd8eb1cc6bfee1e2ed1fb54c
Nick Melms <nick@craftedagency.com>
2025-01-27 13:26:52 -0500

    create validator class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -2,19 +2,20 @@
 
 $heading = "Create Note";
 
+require 'Validator.php';
 $config = require('config.php');
+
+
 $db = new Database($config['database']);
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
   $errors = [];
 
-  if(strlen($_POST['body']) === 0){
-    $errors['body'] = 'a body is required';
-  }else if(strlen($_POST['body']) > 100){
-    $errors['body'] = 'body cannot be more than 1000 characters';
+  if(!Validator::string($_POST['body'], 1, 1000)){
+    $errors['body'] = 'A body of no more than 1000 chars is required';
   }
 
-  if(empty($errors)){
+  if(empty($errors)){ 
       $db->query('insert into notes(body,user_id) values(:body, :user_id)', [
         'body' => $_POST['body'],
         'user_id' => 1,
