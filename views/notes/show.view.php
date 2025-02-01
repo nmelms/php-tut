dcf8baf33bd1ac99d37f2e67c231415df14e6e28
Nick Melms <nick@craftedagency.com>
2025-01-31 20:20:27 -0500

    create update route

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -10,11 +10,17 @@
 
         <p><?= htmlspecialchars($note['body']) ?></p>
 
-        <form class="mt-6" method="POST">
+
+
+        <footer class="mt-6">
+            <a class="text-gray-500 border border-current px-3 py-1 rounded" href="/note/edit?id=<?= $note['id'] ?>">Edit</a>            
+        </footer>
+
+        <!-- <form class="mt-6" method="POST">
             <input type="hidden" name="_method" value="DELETE">
             <input type="hidden" name="id" value="<?= $note['id'] ?>">
             <button class="text-sm text-red-500">Delete</button>
-        </form>
+        </form> -->
     </div>
 </main>
 
