102109b34926628f729f45c81a2c1a870d941399
Nick Melms <nick@craftedagency.com>
2025-02-05 19:37:28 -0500

    setup user session

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -1,4 +1,48 @@
 <?php
 
+use Core\Database;
+use Core\Validator;
+use Core\App;
+
 $email = $_POST['email'];
 $password = $_POST['password'];
+$errors = [];
+
+if (!Validator::email($email)) {
+  $errors['email'] = 'please provide a valid email adress';
+}
+if (!Validator::string($password, 3, 255)) {
+  $errors['password'] = 'please provide a valid password';
+}
+
+if(! empty($errors)){
+  return view('registration/create.view.php', [
+    'errors' => $errors,
+  ]);
+}
+
+echo('hello this istesting');
+
+$db = App::resolve(Database::class);
+
+$user = $db->query('select * from users where email = :email', [
+  'email' => $email
+])->find();
+
+if($user){
+
+  header('location: /');
+  die();
+}else{
+  $db->query('insert into users(email, password) values(:email, :password)', [
+    'email'=> $email,
+    'password'=>$password,
+  ]);
+
+  $_SESSION['user'] = [
+    'email' => $email,
+  ];
+
+  header('location: /');
+  die();
+}
--- views/registration/create.view.php
+++ views/registration/create.view.php
@@ -14,7 +14,7 @@
       <div>
         <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
         <div class="mt-2">
-          <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+          <input type="email" name="email" id="email" autocomplete="email"  class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
         </div>
         <?php if (isset($errors['email'])) : ?>
             <p class="text-red-500 text-xs mt-2"><?= $errors['email'] ?></p>
@@ -26,7 +26,7 @@
           <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
         </div>
         <div class="mt-2">
-          <input type="password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+          <input type="password" name="password" id="password" autocomplete="current-password"  class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
         </div>
         <?php if (isset($errors['password'])) : ?>
             <p class="text-red-500 text-xs mt-2"><?= $errors['password'] ?></p>
